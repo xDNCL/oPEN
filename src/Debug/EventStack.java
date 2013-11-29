b8fb1da7bedd83a39e5aeda7a39881177c758ab6
yuki-shuhara <shuhara.y@gmail.com>
2013-11-29 19:37:55 +0900

    create EventStack

--- src/Debug/EventStack.java
+++ src/Debug/EventStack.java
@@ -6,21 +6,41 @@ import edu.mit.blocks.renderable.RenderableBlock;
 
 
 public class EventStack {
-	
-	private int pointer = 0;
-	
+		
 	private ArrayList<RenderableBlock> runBlockList;
 	
 	EventStack(ArrayList<RenderableBlock> runBlockList){
-		
+		for(int i=runBlockList.size()-1; i > 0; i--){
+			this.setEventBlock(runBlockList.get(i));
+		}
 	}
 	
-	public RenderableBlock getNextEvent(){
-		return runBlockList.get(pointer--);
+	public RenderableBlock getEventBlock(){
+		return this.runBlockList.get(runBlockList.size()-1);
 	}
 	
-	public void setEvent
+	public void setEventBlock(RenderableBlock rb){
+		this.runBlockList.add(rb);
+	}
+	
+	/**
+	 * empty: true
+	 * @return
+	 */
+	public boolean isEmpty(){
+		return runBlockList.size() == 0 ? true : false;
+	}
 	
 	
-
+	@Override
+	public String toString(){
+		String result = "";
+		for(RenderableBlock rb: this.runBlockList){
+			result += "BlockID:"+rb.getBlockID()
+					+"\nGenusName:"+rb.getGenus()
+					+"\n";
+		}
+		return result;
+	}
+	
 }
